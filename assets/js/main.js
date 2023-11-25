54c828fe31747fa9947c6bd7cc0c15b91c59620e
Elitpeaper <ahmetaliemeksiz@gmail.com>
2023-11-25 23:43:30 +0300

    bla bla 2

--- assets/js/main.js
+++ assets/js/main.js
@@ -117,3 +117,187 @@ $('.button-box').click(function () {
       }
     },
   });
+
+
+let constrain = 75;
+let mouseOverContainer1 = document.getElementById("ex1");
+let ex1Layer1 = document.getElementById("ex1-layer");
+
+let mouseOverContainer2 = document.getElementById("ex2");
+let ex1Layer2 = document.getElementById("ex2-layer");
+
+let mouseOverContainer3 = document.getElementById("ex3");
+let ex1Layer3 = document.getElementById("ex3-layer");
+
+let mouseOverContainer4 = document.getElementById("ex4");
+let ex1Layer4 = document.getElementById("ex4-layer");
+
+let mouseOverContainer5 = document.getElementById("ex5");
+let ex1Layer5 = document.getElementById("ex5-layer");
+
+let mouseOverContainer6 = document.getElementById("ex6");
+let ex1Layer6 = document.getElementById("ex6-layer");
+
+
+2
+function transforms(x, y, el) {
+  let box = el.getBoundingClientRect();
+  let calcX = -(x - box.x - (box.height / 2)) / constrain;
+  let calcY = (y - box.y - (box.width / 2)) / constrain;
+  
+  return "perspective(100px) "
+    + "   rotateX("+ calcY +"deg) "
+    + "   rotateY("+ calcX +"deg) ";
+};
+
+ function transformElement(el, xyEl) {
+  el.style.transform  = transforms.apply(null, xyEl);
+}
+
+mouseOverContainer1.onmousemove = function(e) {
+  let xy = [e.clientX, e.clientY];
+  let position = xy.concat([ex1Layer1]);
+
+  window.requestAnimationFrame(function(){
+    transformElement(ex1Layer1, position);
+  });
+};
+
+
+
+function transforms(x, y, el) {
+  let box = el.getBoundingClientRect();
+  let calcX = -(x - box.x - (box.height / 2)) / constrain;
+  let calcY = (y - box.y - (box.width / 2)) / constrain;
+  
+  return "perspective(100px) "
+    + "   rotateX("+ calcY +"deg) "
+    + "   rotateY("+ calcX +"deg) ";
+};
+
+ function transformElement(el, xyEl) {
+  el.style.transform  = transforms.apply(null, xyEl);
+}
+
+mouseOverContainer2.onmousemove = function(e) {
+  let xy = [e.clientX, e.clientY];
+  let position = xy.concat([ex1Layer2]);
+
+  window.requestAnimationFrame(function(){
+    transformElement(ex1Layer2, position);
+  });
+};
+
+
+
+
+
+
+
+function transforms(x, y, el) {
+  let box = el.getBoundingClientRect();
+  let calcX = -(x - box.x - (box.height / 2)) / constrain;
+  let calcY = (y - box.y - (box.width / 2)) / constrain;
+  
+  return "perspective(100px) "
+    + "   rotateX("+ calcY +"deg) "
+    + "   rotateY("+ calcX +"deg) ";
+};
+
+ function transformElement(el, xyEl) {
+  el.style.transform  = transforms.apply(null, xyEl);
+}
+
+mouseOverContainer3.onmousemove = function(e) {
+  let xy = [e.clientX, e.clientY];
+  let position = xy.concat([ex1Layer3]);
+
+  window.requestAnimationFrame(function(){
+    transformElement(ex1Layer3, position);
+  });
+};
+
+
+
+
+
+
+
+function transforms(x, y, el) {
+  let box = el.getBoundingClientRect();
+  let calcX = -(x - box.x - (box.height / 2)) / constrain;
+  let calcY = (y - box.y - (box.width / 2)) / constrain;
+  
+  return "perspective(100px) "
+    + "   rotateX("+ calcY +"deg) "
+    + "   rotateY("+ calcX +"deg) ";
+};
+
+ function transformElement(el, xyEl) {
+  el.style.transform  = transforms.apply(null, xyEl);
+}
+
+mouseOverContainer4.onmousemove = function(e) {
+  let xy = [e.clientX, e.clientY];
+  let position = xy.concat([ex1Layer4]);
+
+  window.requestAnimationFrame(function(){
+    transformElement(ex1Layer4, position);
+  });
+};
+
+
+
+
+
+
+function transforms(x, y, el) {
+  let box = el.getBoundingClientRect();
+  let calcX = -(x - box.x - (box.height / 2)) / constrain;
+  let calcY = (y - box.y - (box.width / 2)) / constrain;
+  
+  return "perspective(100px) "
+    + "   rotateX("+ calcY +"deg) "
+    + "   rotateY("+ calcX +"deg) ";
+};
+
+ function transformElement(el, xyEl) {
+  el.style.transform  = transforms.apply(null, xyEl);
+}
+
+mouseOverContainer5.onmousemove = function(e) {
+  let xy = [e.clientX, e.clientY];
+  let position = xy.concat([ex1Layer5]);
+
+  window.requestAnimationFrame(function(){
+    transformElement(ex1Layer5, position);
+  });
+};
+
+
+
+
+
+
+function transforms(x, y, el) {
+  let box = el.getBoundingClientRect();
+  let calcX = -(x - box.x - (box.height / 2)) / constrain;
+  let calcY = (y - box.y - (box.width / 2)) / constrain;
+  
+  return "perspective(100px) "
+    + "   rotateX("+ calcY +"deg) "
+    + "   rotateY("+ calcX +"deg) ";
+};
+
+ function transformElement(el, xyEl) {
+  el.style.transform  = transforms.apply(null, xyEl);
+}
+
+mouseOverContainer6.onmousemove = function(e) {
+  let xy = [e.clientX, e.clientY];
+  let position = xy.concat([ex1Layer6]);
+
+  window.requestAnimationFrame(function(){
+    transformElement(ex1Layer6, position);
+  });
+};
